b1e4fa6d318ade5658f3e7f20b8a9fee88b51193
Nazim <crazy25000@gmail.com>
2021-03-28 02:00:59 -0400

    REF: Breakup more methods

--- backtesting/backtesting.py
+++ backtesting/backtesting.py
@@ -1,28 +1,29 @@
-"""
-Core framework data structures.
-Objects from this module can also be imported from the top-level
-module directly, e.g.
-
-    from backtesting import Backtest, Strategy
-"""
 import multiprocessing as mp
 import os
 import warnings
 from concurrent.futures import ProcessPoolExecutor, as_completed
 from functools import lru_cache, partial
 from itertools import repeat, product, compress
-from numbers import Number
-from typing import Callable, Dict, List, Sequence, Tuple, Type, Union
+from typing import Callable, Dict, List, Tuple, Type, Union
 
 import numpy as np
 import pandas as pd
-from tqdm.auto import tqdm as _tqdm
 from skopt.space import Integer, Real, Categorical
-from backtesting.broker import _OutOfMoneyError, Broker
+from tqdm.auto import tqdm as _tqdm
+
+from backtesting.broker import Broker
 from backtesting.strategy import Strategy
 from ._plotting import plot
-from ._util import _Indicator, _Data, try_
-from .metrics import compute_stats
+from ._util import _Indicator, _Data
+from .backtesting_helpers import (
+    get_grid_frac,
+    get_max_tries,
+    AttrDict,
+    _tuple,
+    validate_and_get_data,
+    validate_instance_types,
+    loop_through_data,
+)
 
 
 class Backtest:
@@ -94,6 +95,7 @@ class Backtest:
 
             def maximize(stats: pd.Series, _key=maximize):
                 return stats[_key]
+
         elif not callable(maximize):
             raise TypeError(
                 '`maximize` must be str (a field of backtest.run() result '
@@ -103,8 +105,10 @@ class Backtest:
 
         have_constraint = bool(constraint)
         if constraint is None:
+
             def constraint(_):
                 return True
+
         elif not callable(constraint):
             raise TypeError(
                 '`constraint` must be a function that accepts a dict '
@@ -281,23 +285,6 @@ class Backtest:
                 dimensions.append(Categorical(values.tolist(), name=key, transform='onehot'))
         return dimensions
 
-    def get_stats_for_maximized(self, maximize):
-        if isinstance(maximize, str):
-            self.maximize_key = str(maximize)
-            stats = self._results if self._results is not None else self.run()
-            if maximize not in stats:
-                raise ValueError('`maximize`, if str, must match a key in pd.Series result of backtest.run()')
-
-            def maximize(stats: pd.Series, _key=maximize):
-                return stats[_key]
-
-        elif not callable(maximize):
-            raise TypeError(
-                '`maximize` must be str (a field of backtest.run() result '
-                'Series) or a function that accepts result Series '
-                'and returns a number; the higher the better'
-            )
-
     @staticmethod
     def _mp_task(backtest_uuid, batch_index):
         bt, param_batches, maximize_func = Backtest._mp_backtests[backtest_uuid]
@@ -307,7 +294,25 @@ class Backtest:
 
     _mp_backtests: Dict[float, Tuple['Backtest', List, Callable]] = {}
 
-    def plot(self, *, results: pd.Series = None, filename=None, plot_width=None, plot_equity=True, plot_return=False, plot_pl=True, plot_volume=True, plot_drawdown=False, smooth_equity=False, relative_equity=True, superimpose: Union[bool, str] = True, resample=True, reverse_indicators=False, show_legend=True, open_browser=True,):
+    def plot(
+        self,
+        *,
+        results: pd.Series = None,
+        filename=None,
+        plot_width=None,
+        plot_equity=True,
+        plot_return=False,
+        plot_pl=True,
+        plot_volume=True,
+        plot_drawdown=False,
+        smooth_equity=False,
+        relative_equity=True,
+        superimpose: Union[bool, str] = True,
+        resample=True,
+        reverse_indicators=False,
+        show_legend=True,
+        open_browser=True,
+    ):
         if results is None:
             if self._results is None:
                 raise RuntimeError('First issue `backtest.run()` to obtain results.')
@@ -332,114 +337,3 @@ class Backtest:
             show_legend=show_legend,
             open_browser=open_browser,
         )
-
-
-def get_grid_frac(max_tries, _grid_size):
-    if max_tries is None:
-        return 1
-    elif 0 < max_tries <= 1:
-        return max_tries
-
-    return max_tries / _grid_size()
-
-
-def get_max_tries(max_tries, _grid_size):
-    if max_tries is None:
-        return 200
-    elif 0 < max_tries <= 1:
-        return max(1, int(max_tries * _grid_size()))
-
-    return max_tries
-
-
-class AttrDict(dict):
-    def __getattr__(self, item):
-        return self[item]
-
-
-def grid_size(kwargs, _tuple, constraint, have_constraint):
-    size = np.prod([len(_tuple(v)) for v in kwargs.values()])
-    if size < 10_000 and have_constraint:
-        size = sum(1 for p in product(*(zip(repeat(k), _tuple(v)) for k, v in kwargs.items())) if constraint(AttrDict(p)))
-    return size
-
-
-def _tuple(x: List[str]) -> List[str]:
-    return x if isinstance(x, Sequence) and not isinstance(x, str) else (x,)
-
-
-def validate_and_get_data(cash, data):
-    data = data.copy(deep=False)
-    largest_numeric_index = data.index.is_numeric() and (data.index > pd.Timestamp('1975').timestamp()).mean() > 0.8
-    if not isinstance(data.index, pd.DatetimeIndex) and not isinstance(data.index, pd.RangeIndex) and largest_numeric_index:
-        try:
-            data.index = pd.to_datetime(data.index, infer_datetime_format=True)
-        except ValueError:
-            pass
-    if 'Volume' not in data:
-        data['Volume'] = np.nan
-    if len(data) == 0:
-        raise ValueError('OHLC `data` is empty')
-    if len(data.columns.intersection({'Open', 'High', 'Low', 'Close', 'Volume'})) != 5:
-        raise ValueError(
-            '`data` must be a pandas.DataFrame with columns ' "'Open', 'High', 'Low', 'Close', and (optionally) 'Volume'")
-    if data[['Open', 'High', 'Low', 'Close']].isnull().values.any():
-        raise ValueError(
-            'Some OHLC values are missing (NaN). '
-            'Please strip those lines with `df.dropna()` or '
-            'fill them in with `df.interpolate()` or whatever.'
-        )
-    if np.any(data['Close'] > cash):
-        warnings.warn(
-            'Some prices are larger than initial cash value. Note that fractional '
-            'trading is not supported. If you want to trade Bitcoin, '
-            'increase initial cash, or trade μBTC or satoshis instead (GH-134).',
-            stacklevel=2,
-        )
-    if not data.index.is_monotonic_increasing:
-        warnings.warn('Data index is not sorted in ascending order. Sorting.', stacklevel=2)
-        data = data.sort_index()
-    if not isinstance(data.index, pd.DatetimeIndex):
-        warnings.warn(
-            'Data index is not datetime. Assuming simple periods, but `pd.DateTimeIndex` is advised.',
-            stacklevel=2,
-        )
-    return data
-
-
-def validate_instance_types(commission, data, strategy):
-    if not (isinstance(strategy, type) and issubclass(strategy, Strategy)):
-        raise TypeError('`strategy` must be a Strategy sub-type')
-    if not isinstance(data, pd.DataFrame):
-        raise TypeError('`data` must be a pandas.DataFrame with columns')
-    if not isinstance(commission, Number):
-        raise TypeError('`commission` must be a float value, percent of entry order price')
-
-
-def get_perf_metrics(broker, data, strategy, self_data):
-    data._set_length(len(self_data))
-    equity = pd.Series(broker._equity).bfill().fillna(broker._cash).values
-    results = compute_stats(trades=broker.closed_trades, equity=equity, ohlc_data=self_data, risk_free_rate=0.0)
-    results.loc['_strategy'] = strategy
-    return results
-
-
-def loop_through_data(broker, data, indicator_attrs, start, strategy, self_data):
-    with np.errstate(invalid='ignore'):
-        for i in range(start, len(self_data)):
-            data._set_length(i + 1)
-            for attr, indicator in indicator_attrs:
-                setattr(strategy, attr, indicator[..., : i + 1])
-            try:
-                broker.next()
-            except _OutOfMoneyError:
-                break
-
-            strategy.next()
-        else:
-            for trade in broker.trades:
-                trade.close()
-            if start < len(self_data):
-                try_(broker.next, exception=_OutOfMoneyError)
-
-        return get_perf_metrics(broker, data, strategy, self_data)
---
+++ backtesting/backtesting_helpers.py
@@ -0,0 +1,115 @@
+import warnings
+from numbers import Number
+from typing import List, Sequence
+
+import numpy as np
+import pandas as pd
+
+from backtesting.backtesting import Strategy
+from backtesting._util import try_
+from backtesting.broker import _OutOfMoneyError
+from backtesting.metrics import compute_stats
+
+
+def get_grid_frac(max_tries, _grid_size):
+    if max_tries is None:
+        return 1
+    elif 0 < max_tries <= 1:
+        return max_tries
+
+    return max_tries / _grid_size()
+
+
+def get_max_tries(max_tries, _grid_size):
+    if max_tries is None:
+        return 200
+    elif 0 < max_tries <= 1:
+        return max(1, int(max_tries * _grid_size()))
+
+    return max_tries
+
+
+class AttrDict(dict):
+    def __getattr__(self, item):
+        return self[item]
+
+
+def _tuple(x: List[str]) -> List[str]:
+    return x if isinstance(x, Sequence) and not isinstance(x, str) else (x,)
+
+
+def validate_and_get_data(cash, data):
+    data = data.copy(deep=False)
+    largest_numeric_index = data.index.is_numeric() and (data.index > pd.Timestamp('1975').timestamp()).mean() > 0.8
+    if not isinstance(data.index, pd.DatetimeIndex) and not isinstance(data.index, pd.RangeIndex) and largest_numeric_index:
+        try:
+            data.index = pd.to_datetime(data.index, infer_datetime_format=True)
+        except ValueError:
+            pass
+    if 'Volume' not in data:
+        data['Volume'] = np.nan
+    if len(data) == 0:
+        raise ValueError('OHLC `data` is empty')
+    if len(data.columns.intersection({'Open', 'High', 'Low', 'Close', 'Volume'})) != 5:
+        raise ValueError(
+            '`data` must be a pandas.DataFrame with columns ' "'Open', 'High', 'Low', 'Close', and (optionally) 'Volume'")
+    if data[['Open', 'High', 'Low', 'Close']].isnull().values.any():
+        raise ValueError(
+            'Some OHLC values are missing (NaN). '
+            'Please strip those lines with `df.dropna()` or '
+            'fill them in with `df.interpolate()` or whatever.'
+        )
+    if np.any(data['Close'] > cash):
+        warnings.warn(
+            'Some prices are larger than initial cash value. Note that fractional '
+            'trading is not supported. If you want to trade Bitcoin, '
+            'increase initial cash, or trade μBTC or satoshis instead (GH-134).',
+            stacklevel=2,
+        )
+    if not data.index.is_monotonic_increasing:
+        warnings.warn('Data index is not sorted in ascending order. Sorting.', stacklevel=2)
+        data = data.sort_index()
+    if not isinstance(data.index, pd.DatetimeIndex):
+        warnings.warn(
+            'Data index is not datetime. Assuming simple periods, but `pd.DateTimeIndex` is advised.',
+            stacklevel=2,
+        )
+    return data
+
+
+def validate_instance_types(commission, data, strategy):
+    if not (isinstance(strategy, type) and issubclass(strategy, Strategy)):
+        raise TypeError('`strategy` must be a Strategy sub-type')
+    if not isinstance(data, pd.DataFrame):
+        raise TypeError('`data` must be a pandas.DataFrame with columns')
+    if not isinstance(commission, Number):
+        raise TypeError('`commission` must be a float value, percent of entry order price')
+
+
+def get_perf_metrics(broker, data, strategy, self_data):
+    data._set_length(len(self_data))
+    equity = pd.Series(broker._equity).bfill().fillna(broker._cash).values
+    results = compute_stats(trades=broker.closed_trades, equity=equity, ohlc_data=self_data, risk_free_rate=0.0)
+    results.loc['_strategy'] = strategy
+    return results
+
+
+def loop_through_data(broker, data, indicator_attrs, start, strategy, self_data):
+    with np.errstate(invalid='ignore'):
+        for i in range(start, len(self_data)):
+            data._set_length(i + 1)
+            for attr, indicator in indicator_attrs:
+                setattr(strategy, attr, indicator[..., : i + 1])
+            try:
+                broker.next()
+            except _OutOfMoneyError:
+                break
+
+            strategy.next()
+        else:
+            for trade in broker.trades:
+                trade.close()
+            if start < len(self_data):
+                try_(broker.next, exception=_OutOfMoneyError)
+
+        return get_perf_metrics(broker, data, strategy, self_data)
